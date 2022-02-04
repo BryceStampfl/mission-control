c0e221b4fbf0b097612204b0c7e4cacfea187717
BryceStampfl <BryceStampfl@gmail.com>
2022-02-03 19:05:34 -0800

    initial setup for frontend

---
+++ client/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+/*
+entry/output: tells our server what has to be compiled and from where
+resolve: allows us to import anything from the 'src' folder in relative instead of absolute
+devserver: tells the webpack-dev-server what files are needed to be served
+plugin: what plugins we need for our app
+*/
+module.exports = {
+    entry: path.join(__dirname, "src", "index.js"),
+    output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+    mode: process.env.NODE_ENV || "development",
+    resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
+    devServer: { static: path.join(__dirname, "src") },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: { presets: ['@babel/env','@babel/preset-react'] },
+              },
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader"],
+            },
+            { 
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ["file-loader"] 
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html"),
+        }),
+    ],
+};
